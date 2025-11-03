7cf4ddf9e90ec0cc959974de7cc6f430c671103c
Zane Kundzina <zane.gaiduka@gmail.com>
2025-11-03 17:06:16 +0200

    Added product model, products, product detail view

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('VG', 'Vegetables'),
+    ('FR', 'Fruits'),
+    ('GR', 'Grains'),
+    ('DP', 'Dairy Products'),
+    ('MT', 'Meat'),
+    ('NS', 'Nuts and Seeds'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home),
-]
+    path("", views.home),       
+    path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
+    path("product/<int:pk>", views.ProductDetailView.as_view(), name="product"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
